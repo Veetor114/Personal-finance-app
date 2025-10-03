3938fa3c168acd0546fa4fbc0472cff7a216a482
Victor <162317625+Veetor114@users.noreply.github.com>
2025-10-03 03:00:24 +0100

    Delete Personal Finance App Design directory

--- Personal Finance App Design/src/utils/supabase/client.tsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { projectId, publicAnonKey } from './info';
-
-export const supabase = createClient(
-  `https://${projectId}.supabase.co`,
-  publicAnonKey
-);
